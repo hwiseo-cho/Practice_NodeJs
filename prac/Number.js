e6c30e6d66cdf0ac6a0e358273aebd78c94dbd54
hwiseo-cho <gnltj37@naver.com>
2020-12-04 21:35:09 +0900

    commit

---
+++ prac/Number.js
@@ -0,0 +1,9 @@
+console.log(1);
+console.log(4-1);
+console.log(4*2);
+console.log(10/2);
+console.log('----------------------');
+console.log("문자");
+console.log("1"+'1');
+var str = 'sfdaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd';
+console.log(str.length);
---
+++ prac/template.js
@@ -0,0 +1,7 @@
+var str = 'fdsaaaaaaaaaaaaaaddssssssssssssssssss';
+var name = 'hihi';
+// '' 말고 --> ``로 바꾸면 이런식으로 변수를 ${}의 형태로 출력 가능
+var letter = `Dear ${name} 
+
+dsdf ${1+1}`;
+console.log(letter);
